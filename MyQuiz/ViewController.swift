69a05ffc71f8ab72f85c330b5d9473bf47e712f3
goki sakaguchi <goki0524@gmail.com>
2019-05-02 13:39:57 +0900

    first commit

---
+++ MyQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyQuiz
+//
+//  Created by 坂口豪紀 on 2019/05/02.
+//  Copyright © 2019 Goki Sakaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
